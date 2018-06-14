d02283cae9237ff5345c11729535f6fbf6490be4
Jackson L. Cole <jackson.lane.cole@gmail.com>
2018-06-14 01:13:31 -0500

    Imported questions.models in case I want to test some stuff

--- questions/tests.py
+++ questions/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from questions.models import (
+        Course,
+        Book,
+        Chapter,
+        Objective,
+        Source,
+        Question,
+        MultipleChoiceQuestion,
+        TrueFalseQuestion,
+        QuestionBlock,
+        QuestionBlockQuestion,
+        QuestionBlockQuestionAnswer,
+        )
 
 # Create your tests here.
