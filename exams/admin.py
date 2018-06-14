ad6db6a919f815c77f2328cd17edb2554194ed06
Jackson L. Cole <jackson.lane.cole@gmail.com>
2018-06-14 01:09:12 -0500

    Added registration of MasterExam model

--- exams/admin.py
+++ exams/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import (
+        MasterExam,
+        )
 
 # Register your models here.
+
+###############################################################################
+# REGISTRATION
+admin.site.register(
+        MasterExam,
+        )
+
+###############################################################################
