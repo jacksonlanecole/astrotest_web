6837789cbf9c900bba26bd6d28dee10e05a88453
Jackson L. Cole <jackson.lane.cole@gmail.com>
2018-06-14 01:14:07 -0500

    Yet another 'probably doesn't matter right now' commit

--- questions/views.py
+++ questions/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from .models import Question
+from .models import Question, Objective
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required
 def question_list(request):
     #if request.method == 'POST':
-    questions = Question.objects.all()
+    objectives = Objective.objects.all()
+    #questions = Question.objects.all()
     return render(request, "questions/question_list.html",
-            {"questions":questions})
+            {"objectives":objectives})
 
 
 @login_required
